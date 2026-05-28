96e5de7ecf8dbc998496dd1cf5560830683295b8
Vikum Karunathilake <vikumbk@yahoo.com>
2026-05-28 14:01:41 +0530

    fix(build): resolve windows code signing ci hang and update build automation config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli'
 
 const config: CapacitorConfig = {
-  appId: 'com.cafeverse.app',
+  appId: 'io.github.vikumkarunathilake.cafeverse',
   appName: 'CaféVerse',
   webDir: 'out/renderer'
 }
