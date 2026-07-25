c7f212e920357bcba21e5e08dd64031181f06e9d
Vikum Karunathilake <vikumbk@yahoo.com>
2026-07-26 02:34:16 +0530

    refactor: update build configuration to include all project files and simplify dependency externalization

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -4,25 +4,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   main: {
-    plugins: [
-      externalizeDepsPlugin({
-        exclude: [
-          '@electron-toolkit/utils',
-          '@electron-toolkit/preload',
-          '@ghostery/adblocker-electron',
-          '@xhayper/discord-rpc',
-          'cross-fetch',
-          'electron-updater'
-        ]
-      })
-    ]
+    plugins: [externalizeDepsPlugin()]
   },
   preload: {
-    plugins: [
-      externalizeDepsPlugin({
-        exclude: ['@electron-toolkit/utils', '@electron-toolkit/preload']
-      })
-    ]
+    plugins: [externalizeDepsPlugin()]
   },
   renderer: {
     resolve: {
