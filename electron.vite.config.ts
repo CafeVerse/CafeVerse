ea12684857875c67f7ea70d4531dadfbc419868e
Vikum Karunathilake <vikumbk@yahoo.com>
2026-07-26 02:19:12 +0530

    chore: configure externalizeDepsPlugin with package exclusions in main and preload build configurations

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,10 +1,29 @@
 import { resolve } from 'path'
-import { defineConfig } from 'electron-vite'
+import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  main: {},
-  preload: {},
+  main: {
+    plugins: [
+      externalizeDepsPlugin({
+        exclude: [
+          '@electron-toolkit/utils',
+          '@electron-toolkit/preload',
+          '@ghostery/adblocker-electron',
+          '@xhayper/discord-rpc',
+          'cross-fetch',
+          'electron-updater'
+        ]
+      })
+    ]
+  },
+  preload: {
+    plugins: [
+      externalizeDepsPlugin({
+        exclude: ['@electron-toolkit/utils', '@electron-toolkit/preload']
+      })
+    ]
+  },
   renderer: {
     resolve: {
       alias: {
